21f8579e36222fb3c4dd985fe82880610ce02460
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2021-11-02 04:27:52 +0100

    Add files via upload

---
+++ src/Area.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import javax.swing.*;  
+import java.awt.event.*;
+public class Area 
+{
+    private JFrame f;
+    public Area()
+    {
+        f = App.giveframe();
+        System.out.println(f);
+    }
+    public void calculation()
+    {
+        //функция которая будет сравнивать 1-ое и второе текстовое поле
+        //должен быть под метод который в свою очередь будет иметь два стринга, 1-ый отвечает за 1 поле, второй за второе (вынести в метод)
+        //calculation считает и говорит какое неправильно выделяя цветом
+        //подумать или посмотреть про статик что-то выглядит странно
+    }
+}
