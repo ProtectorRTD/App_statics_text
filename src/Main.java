95828bb6b2ccd864119b4ef4982ead349faac401
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2021-11-03 03:23:58 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import javax.swing.*;  
+import java.awt.event.*;
+public class Main 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        App app = new App();
+        //Area area = new Area();
+    }
+
+}
