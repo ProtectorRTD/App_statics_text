3e472605da3131d31bdf6efc57cf3f9b409c1260
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2021-11-09 02:21:14 +0100

    Update Area.java
    -я говноед признаю
    - Комменты для слабаков только говнокод
    - если что Никита пиши

--- src/Area.java
+++ src/Area.java
@@ -1,5 +1,6 @@
 import java.security.KeyStore.Entry;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -20,47 +21,170 @@ public class Area
     private App app;
     private boolean to_the_end;
     private JTextArea area;
+    private Set<String> dictionary;
     public Area(App app)
     {
         this.app = app;
+        dictionary = new HashSet<String>();
+        putInDictionary();
     }
     public void calculation(JTextArea area)
     {
-        // 1) Три самых популярных слова
+        // 1) Три самых популярных слова +- сделан функционал, вроде работае правильно
         // 2) Самая часта буква
-        // 3) Количество слов
-        // 4) Самая редкая буква
+        // 3) Количество слов + (сделано)
+        // 4) Самая редкая буква + сделано + самая популярная буква
         // 5) Самое редкое слово
         // 6) Самое короткое слово
-        // 7) Самое длинное слово
-        // 8) Подсчет каждой буквы
-        // 9) Количество абзаце
+        // 7) Самое длинное слово + (сделано) 
+        // 8) Подсчет каждой буквы самая частая буква 
+        // 9) Количество абзацев (не совсем понятно, что именно считается через \n + \t или другое)
         // 10) Количество матов
         // 11) Подсчёт по группам : диеслово имэныкы
         // 12) Поиск по тексту
         // 13) Количество родов слов
         // 14) Количество символов
+        // 15) Количество союзов + сделано (но нужно потом для англ )
+        // 16) Количество воды
+        // 17) Количество уникальных слоформ
+        // 18) Количество каждого уникального слова
+        // 19) Какие буквы вообще не юзали
 
+        // считать слова через тхт файл
+        // также проверять слова в тхт файле на большое и маленькое
+        // подправить вывод троих слов
+        //Там где написано Рефакторинг означает что придется работать с потоками
+        // придумать метод в котором открывается поток передается буква и он ищет, начиная с этой буквы 
         this.area = area;
         firstText = app.getFirst();
         secondText = app.getSecond();
         popularWord();
-        
+        onlyWord();
+        topCharacters();
         
 
         
         //12398456 123456 должен выделить 98
     }
+    private void topCharacters()
+    {
+        int count = 0;
+        int min = Integer.MAX_VALUE;
+        HashMap<Character, Integer> hash_map = new HashMap<Character, Integer>();
+        for(int i = 0; i < firstText.length(); i++) // пройтись циклом по всему массиву 
+        { 
+            if(firstText.charAt(i) >= 'A' && 'Z'<= firstText.charAt(i) || firstText.charAt(i) >= 'a' && 'z'<= firstText.charAt(i))
+            {
+                if(hash_map.containsKey(firstText.charAt(i)) == true) 
+                {
+                            //создать функцию которая не будет записывать какие-то частицы
+                            count = hash_map.get(firstText.charAt(i));
+                            count++;
+                            hash_map.replace(firstText.charAt(i), count); // заменять с таким же на 1 больше, было 2 раза а станет 3
+                }
+                else 
+                {
+                            hash_map.put(firstText.charAt(i), 1);
+                }                       
+            }              
+            count = Integer.MIN_VALUE;
+            for(int value : hash_map.values())
+            {
+                if(count < value)
+                {
+                    count = value;                    
+                }
+                if(min > value)
+                {
+                    min = value;
+                }
+            }
+        }
+
+        Character result = getKey(hash_map, count);
+        boolean more_than_one = false;
+        if(result != null)this.area.append("\n"+result+" - самая популярная буква - " + count +", использований\n");
+        while(result != null)
+        {
+            result = getKey(hash_map, count);
+            if(result != null)
+            {
+                if(more_than_one == true) this.area.append(", ");
+                this.area.append(result+"");
+            }
+            more_than_one = true;
+        }
+        printfUses(more_than_one);
+        more_than_one = false;
+        result = getKey(hash_map, min);
+        if(result != null)this.area.append("\n"+result+"- самая (не)популярная буква - " + min + ",использований\n");
+        while(result != null)
+        {
+            result = getKey(hash_map, min);
+            if(result != null)
+            {
+                if(more_than_one == true) this.area.append(", ");
+                this.area.append(result+"");
+            }
+            more_than_one = true;
+        }
+        printfUses(more_than_one);
+    }
+    private void printfUses(boolean more_than_one)
+    {
+        if(more_than_one == true)
+        {
+            this.area.append(" - буквы которые имеют столько же использований");
+        }
+    }
+    private <K, V> K getKey(Map<K, V> map, V value)
+    {
+        for (Map.Entry<K, V> entry: map.entrySet())
+        {
+            if (value.equals(entry.getValue())) 
+            {
+                K result = entry.getKey();
+                //map.remove(entry.getValue());
+                map.remove(entry.getKey());
+                return result;// записать этот entry массив
+            }
+        }
+        return null;
+    }
+    private void theBiggestWord(String word[]) //находить самое длинное слово а ещё и самое короткое нейминг для клоунов
+    {
+        //Refactoring 
+        String longest_word = "";
+        char[] array = new char[1500000];  
+        Arrays.fill(array, 'a');  
+        String smallest_word = new String(array);  
+        for(int i = 0; i < word.length; i++)
+        {
+            if(!dictionary.contains(word[i]) && longest_word.length() < word[i].length())
+            {
+                longest_word = word[i];
+            }
+            if(!dictionary.contains(word[i]) && smallest_word.length() >= word[i].length())
+            {
+                smallest_word = word[i];
+            }
+        }
+        this.area.append("\n"+longest_word + " - Самое длинное слово");
+        this.area.append("\n"+smallest_word+ " - Самое короткое слово");
+    }
+    private void count_word(int lenght) //сколько слов в тексте
+    {
+        //refactoring 
+        this.area.append("\n"+lenght + "-Количество слов в тексте");
+    }
     private void popularWord()
     {
-        //привет как дела фиксить
-        //чтобы ещё выводил сколько раз его использовали хм
         HashMap<String, Integer> hash_map = new HashMap<String, Integer>(); //создается карта 
         int count = 0; // счетчик для того чтобы считать какой раз уже записывалось 
-        firstText = firstText.replaceAll("[^a-zA-Z0-9]"," ");
+        firstText = firstText.replaceAll("[^a-zA-Zа-яА-Я0-9]"," ");
         firstText = firstText.replace("  ", " ");
         String[] word = firstText.split(" "); //разделить весь инпут по пробелам 
-        for(int i = 0; i < word.length; i++) // пройтись циклом по всему массиву
+        for(int i = 0; i < word.length; i++) // пройтись циклом по всему массиву 
         {                
             if(hash_map.containsKey(word[i]) == true) 
             {
@@ -71,12 +195,12 @@ public class Area
             }
             else 
             {
+                    //refactoring 
                     hash_map.put(word[i], 1);
             }                       
         }
         Map<String, Integer> hash_map_after_sorting = sortByValue(hash_map); // спизженая функция которая сортирует по ключам 
         String [] arr = hash_map_after_sorting.keySet().toArray(new String [0]); // из хэш мапы делаем в массив
-        // String[] values =hash_map_after_sorting.values().toArray(new String[0]);
         Integer[] values = (hash_map_after_sorting.values().toArray(new Integer[hash_map.values().size()]));
         int start = arr.length - 1;
         this.area.setText("Топ 3 слова - ");
@@ -86,12 +210,22 @@ public class Area
             if(i < 0 ) break;
             if(arr[i] != " ")
             {                
-                this.area.append(arr[i] + " ");
-                this.area.append(values[i] + " ");
-            }
+                this.area.append(arr[i] + "-");
+                this.area.append(values[i] + ", ");
+            }            
             start--;
         }
-        //System.out.println(hash_map);
+        for(int j = 0; j < arr.length-1; j++)
+        {
+            if(arr[j] != " ")
+            {
+                this.area.append("\n"+arr[j] + " - Самое редкое слово");
+                break;
+            }
+        }
+        count_word(word.length);
+        theBiggestWord(word);
+        
     }
     private HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm)
     {
@@ -114,14 +248,26 @@ public class Area
     }
     private void onlyWord() //функция которая будет считать количество союзных слов
     {
-        Set<String> dictionary = new HashSet<String>();
-        dictionary.add("И");
-        dictionary.add("Ни");
+        String[] word = firstText.toLowerCase().split(" ");
+        int count = 0;
+        for(int i = 0; i < word.length; i++)
+        {
+            if(dictionary.contains(word[i]))
+            {
+                count++;
+            }
+        }
+        this.area.append("\n"+ count + " - Количество союзов в тексе");
+    }
+    private void putInDictionary()
+    {
+        dictionary.add("и");
+        dictionary.add("ни");
         dictionary.add("да");
         dictionary.add("тоже");
         dictionary.add("также");
         dictionary.add("а"); //
-        dictionary.add("Но");
+        dictionary.add("но");
         dictionary.add("однако");
         dictionary.add("зато");
         dictionary.add("же");
@@ -142,16 +288,5 @@ public class Area
         dictionary.add("словно");
         dictionary.add("точно");
         dictionary.add("несмотря");
-        String[] word = firstText.split(" ");
-        int count = 0;
-        for(int i = 0; i < firstText.length(); i++)
-        {
-            if(dictionary.contains(word[i]))
-            {
-                count++;
-            }
-        }
-        this.area.append("\n "+ count + "- Количество союзов \n");
     }
-
 }
