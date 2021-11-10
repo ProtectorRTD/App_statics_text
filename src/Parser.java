9e2dd1056349558706f03f3018380a720e4094e7
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2021-11-10 02:36:07 +0100

    Add files via upload

---
+++ src/Parser.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+public class Parser 
+{
+//src/wordforms-russian+.txt
+
+}
